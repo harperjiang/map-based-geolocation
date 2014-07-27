78fae0df6eafc78398bf5ef0eeeccc48214b9e85
Harper Jiang <harperjiang@msn.com>
2014-07-27 11:34:43 -0400

    Change Unit

--- src/edu/clarkson/cs/mbg/map/DistanceUtils.java
+++ src/edu/clarkson/cs/mbg/map/DistanceUtils.java
@@ -4,7 +4,14 @@ import java.math.BigDecimal;
 
 public class DistanceUtils {
 
-	private static final BigDecimal factor = BigDecimal.TEN;
+	private static final BigDecimal factor = new BigDecimal(10000);
+
+	/**
+	 * From delay(millisecond) to distance(meter),
+	 * 
+	 * @param input
+	 * @return
+	 */
 
 	public static BigDecimal timeToDistance(BigDecimal input) {
 		return input.multiply(factor);
